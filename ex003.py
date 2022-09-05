5f1bcdd45bd24ea249602f55b00ee92d112cd314
João Victor Antuch <johnantuch@gmail.com>
2022-09-05 10:49:58 -0300

    ex003 Ints

---
+++ ex003.py
@@ -0,0 +1,12 @@
+# Not int
+n1 = input('Choose a number')
+n2 = input('Choose another number')
+result = n1 + n2
+print('The sum is {}'.format(result))
+
+#Int
+n3 = int(input('Choose a number'))
+n4 = int(input('Choose another one'))
+
+result2 = n3 + n4
+print('The sum is {}'.format(result2))
